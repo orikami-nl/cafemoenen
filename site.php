3c8947d8405a605d248354d653a366be3a1de806
Luc Vandewall <lucvandewall@gmail.com>
2012-06-01 10:23:26 +0200

    mobile logo

---
+++ site.php
@@ -0,0 +1,7 @@
+<?php 
+define('SERVER_PATH_VARIABLE', 'REDIRECT_URL');
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'concrete');
+define('PASSWORD_SALT', 'sbvQrqLOXQJqTj9FLZfrN0UHSwTPRuu5GHw5o6Cvn1xmsPPcveublVH7KXeGug3Z');
